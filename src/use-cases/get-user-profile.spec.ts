f8d8342e0763fe72c69d9babb5ef9d1d000f47fb
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-12 14:33:54 -0300

    Implementando casos de uso - Caso de uso de perfil

--- src/use-cases/get-user-profile.spec.ts
+++ src/use-cases/get-user-profile.spec.ts
@@ -1,10 +1,7 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
-import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { GetUserProfileUseCase } from './get-user-profile'
-import { create } from 'domain'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let usersRepository: InMemoryUsersRepository
